071bbe56052e82c11c1e6c5ff5dea3a6861a0eee
derikhill <derikhill05@gmail.com>
2014-10-26 20:58:30 -0500

    added more js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
+	playTheme();
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		$('.instructions').show();
 	})
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+		$('.instructions').hide();
 	})
 	.mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show()
 		$('.hadouken').finish().show()
 			.animate(
 				{'left': '300px'},
@@ -23,13 +25,33 @@ $(document).ready(function() {
 		);
 	})
 	.mouseup(function() {
-		// Ryu goes back to ready position //
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	$(document).keydown(function(e) {	
+		if (e.keyCode == 88) {
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide(); 
+		$('.ryu-throwing').hide(); 
+		$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(e) {
+		if (e.keyCode == 88) {
+		$('.ryu-still').show();
+		$('.ryu-cool').hide();
+		};
 });
+	
 	function playHadouken () {
 	$('#hadouken-sound') [0].volume = 0.5;
 	$('#hadouken-sound') [0].load();
 	$('#hadouken-sound') [0].play();	
 	}
+
+	function playTheme () {
+	$('#theme-music') [0].volume = 0.4;
+	$('#theme-music') [0].load();
+	$('#theme-music') [0].play();
+	}
+});	
